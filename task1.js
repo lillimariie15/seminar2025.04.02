82580d293e8731c0642f3bdfb06d83ecd86f89f3
bachinskim <250670+bachinskim@users.noreply.github.com>
2025-04-01 19:54:13 +0200

    Small update to task1

--- task1.js
+++ task1.js
@@ -1,23 +1,23 @@
 document.getElementById('redBtn').onclick = function() {
     document.body.style.backgroundColor = 'red';
-    console.log('Red button clicked');
+    console.log('red button clicked');
     alert('Red button clicked!');
 };
 
 document.getElementById('greenBtn').onclick = function() {
     document.body.style.backgroundColor = 'green';
-    console.log('Green button clicked');
+    console.log('green button clicked');
     alert('Green button clicked!');
 };
 
 document.getElementById('blueBtn').onclick = function() {
     document.body.style.backgroundColor = 'blue';
-    console.log('Blue button clicked');
+    console.log('blue button clicked');
     alert('Blue button clicked!');
 };
 
 document.getElementById('yellowBtn').onclick = function() {
     document.body.style.backgroundColor = 'yellow';
-    console.log('Yellow button clicked');
+    console.log('yellow button clicked');
     alert('Yellow button clicked!');
 };
